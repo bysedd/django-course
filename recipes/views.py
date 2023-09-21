613eb0ad80052ad313b6b102a9d417c8c391d6cd
Felippe Andrade <4drade@gmail.com>
2023-09-21 16:56:06 -0300

    ✨ feat: Add basic views and HTML templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html', {'name': 'Felippe Andrade'})
+
+
+def about(request):
+    return render(request, 'recipes/about.html')
+
+
+def contact(request):
+    return render(request, 'recipes/contact.html')
